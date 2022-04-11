64388f6f0e57d08a8cc7b0a7e74dc6bbb20ae6cd
Jed Boffey <jedboffey@gmail.com>
2022-04-11 15:06:18 -0400

    Added functionality to strip POS tags

---
+++ corpus-processing/text-processing.py
@@ -0,0 +1,34 @@
+import os
+
+CORPUS_FOLDERS = ["dev-corpus", "test-corpus", "training-corpus"]
+
+def stripPosTags(path):
+    #path is being passed in from a listdir so we shouldn't ever hit this block (possibly redudant?)
+    if not os.path.isfile(path):
+        print("Error: Missing file (" + path + ")")
+        exit() #just close out here and diagnose the problem
+
+    file = open(path)
+    text = file.read()
+
+    processedText = " ".join(word.split("/")[0] for word in text.split())
+
+    #write to new files
+    head, tail = os.path.split(path)
+    head = head + "-processed"
+
+    processedPath = os.path.join(head, tail)
+    with open(processedPath, "w") as file:
+        file.write(processedText)
+
+
+def main():
+    cwd = os.getcwd()
+    for folder in CORPUS_FOLDERS:
+        path = os.path.join(cwd, folder)
+        for file in os.listdir(path):
+            print(file)
+            stripPosTags(os.path.join(path, file))
+
+if __name__ == "__main__":
+    main()
